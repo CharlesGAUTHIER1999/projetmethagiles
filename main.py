52025cec6c8f5f0b13d835c7bd9fe5a65071de63
Mounir <mounirallaoui25@gmail.com>
2024-10-10 10:00:02 +0200

    Clavier ok avec les touches

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from librairie.graphique.graphique_interface_tk import GraphiqueInterfaceTk
-from ecran.ecran_menu import EcranPrincipale
+from ecran.ecran_principal import EcranPrincipal
 from enums.ecran_etat_enum import EcranEtat
 from ecran.getsionnaire_etat_ecran import GestionnaireEtatEcran
-
-from note_frequence_base import note_to_frequency
-
 
 
 if __name__ == "__main__":
@@ -12,11 +9,7 @@ if __name__ == "__main__":
     
     gestionnaire_etat_ecran = GestionnaireEtatEcran()
 
-    ecran_principale = EcranPrincipale(graphic, gestionnaire_etat_ecran)
-    gestionnaire_etat_ecran.ajouter_ecran(EcranEtat.PRINCIPAL, ecran_principale)
-
-    ecran_principale.play(note_to_frequency["F7"], 1)
-    ecran_principale.play(note_to_frequency["B3"], 4)
-    ecran_principale.play(note_to_frequency["E5"], 0.5)
+    ecran_principal = EcranPrincipal(graphic, gestionnaire_etat_ecran)
+    gestionnaire_etat_ecran.ajouter_ecran(EcranEtat.PRINCIPAL, ecran_principal)
 
     gestionnaire_etat_ecran.afficher_etat()
