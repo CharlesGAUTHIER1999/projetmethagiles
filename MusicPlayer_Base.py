73cb4d0e599916a64c5c678585eac4eb5755881e
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2024-10-10 16:34:49 +0200

    tempo ok

--- MusicPlayer_Base.py
+++ MusicPlayer_Base.py
@@ -67,11 +67,11 @@ class MusicPlayer:
         t = np.linspace(0, duration, int(self.sample_rate * duration), False)
 
         # Synthèse additive avec plusieurs harmoniques pour imiter le son d'un piano
-        tone = (0.5 * np.sin(frequency * 2 * np.pi * t) +                    # Fondamentale
-                0.25 * np.sin(2 * frequency * 2 * np.pi * t) +              # 1ère harmonique
-                0.125 * np.sin(3 * frequency * 2 * np.pi * t) +             # 2ème harmonique
-                0.1 * np.sin(4 * frequency * 2 * np.pi * t) +                # 3ème harmonique
-                0.05 * np.sin(5 * frequency * 2 * np.pi * t))                # 4ème harmonique
+        tone = (0.7 * np.sin(frequency * 2 * np.pi * t) +               # Fondamentale
+            0.2 * np.sin(2 * frequency * 2 * np.pi * t) +           # 1ère harmonique
+            0.1 * np.sin(3 * frequency * 2 * np.pi * t) +           # 2ème harmonique
+            0.05 * np.sin(4 * frequency * 2 * np.pi * t) +          # 3ème harmonique
+            0.03 * np.sin(5 * frequency * 2 * np.pi * t))           # 4ème harmonique
 
         # Appliquer l'enveloppe ADSR
         tone_with_adsr = self.apply_adsr_envelope(tone, attack=0.02, decay=0.1, sustain_level=0.5, release=0.2, duration=duration)
@@ -86,22 +86,40 @@ class MusicPlayer:
         pygame.time.delay(int(duration * 1000))
 
     # génère une suite de notes aléatoires
-    def generate_random_sequence(self, num_notes, filename):
+    def generate_random_sequence(self, num_notes, filename, tempo):
         notes = list(note_to_frequency_noires.keys())
-        
         print(notes)
-        notes.append("Unknown")
+        notes.append("0")
+
+        precedente_note_true = False
 
         with open(filename, "w") as f:
             for _ in range(num_notes):
-                note = np.random.choice(notes)
+                if precedente_note_true:
+                    note = "0"
+                else:
+                    note = np.random.choice(notes)
+
+                if note != "0":
+                    precedente_note_true = True
+                else:
+                    precedente_note_true = False
+
+                if tempo == 60:
+                    duration = 1
+                elif tempo == 90:
+                    duration = 0.5
+                elif tempo == 120:
+                    duration = 0.25
+                else:
+                    duration = np.random.uniform(0.25, 1)
                 # duration = np.random.uniform(0.1, 0.5)
-                f.write(f"{note} {0.25}\n")
+                f.write(f"{note} {duration}\n")
 
 
 
     # lecture d'un fichier texte contenant une séquence de notes
-    def play_sequence(self, filename):
+    def play_sequence(self, filename, tempo):
         notes_sequence = []
         duration_sequence = []
 
@@ -117,23 +135,23 @@ class MusicPlayer:
 
             # print(note, duration)
 
-            if (note == "Unknown"):
-                continue
-
-            if (note == "0"):
-                pygame.time.delay(int(duration * 1000))
+            if (note == "0" or note == "Unknown"):
+                pygame.time.delay(int(duration * 1000 * tempo))
                 continue
 
             frequency = note_to_frequency[note]
-            self.play_piano_tone(frequency, duration)
+            self.play_piano_tone(frequency, duration * tempo)
 
 
 
 # Code pour jouer la suite de notes
 if __name__ == "__main__":
     mp = MusicPlayer()
-    # mp.play_sequence("pirate.txt")
-    mp.generate_random_sequence(25, "test.txt")
+    # mp.play_sequence("pirate.txt", 1.5)
+    # mp.generate_random_sequence(25, "test.txt", 60)
+    # mp.generate_random_sequence(25, "test.txt", 90)
+    mp.generate_random_sequence(25, "test.txt", 120)
+    # mp.generate_random_sequence(25, "test.txt", "Aleatoire")
     with open("test.txt", "a") as f:
         f.write("0 0.5\n")
-    mp.play_sequence("test.txt")
+    mp.play_sequence("test.txt", 1)
